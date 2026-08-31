b55ca082a9a23ea8531585bdb01f21ad5f5d1e86
kwabena-aboah <obed30mintah@yahoo.com>
2026-08-31 08:37:11 +0000

    Added WYSIWYG

--- backend/config/wsgi.py
+++ backend/config/wsgi.py
@@ -1,4 +1,19 @@
 import os
+import sys
+from pathlib import Path
+
+# Project directory:
+# /config/
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+if str(BASE_DIR) not in sys.path:
+    sys.path.insert(0, str(BASE_DIR))
+
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE",
+    "config.settings"
+)
+
 from django.core.wsgi import get_wsgi_application
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+
 application = get_wsgi_application()

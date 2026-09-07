43580f183674fbc624539534c95ab57fa4c69021
kwabena-aboah <obed30mintah@yahoo.com>
2026-09-07 18:13:30 +0000

    Fixed email settings

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -188,12 +188,37 @@ EMAIL_BACKEND = config(
     'EMAIL_BACKEND',
     default='django.core.mail.backends.console.EmailBackend' if DEBUG else 'django.core.mail.backends.smtp.EmailBackend',
 )
-DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL', default='noreply@gooprep.com')
-EMAIL_HOST = config('EMAIL_HOST', default='smtp.sendgrid.net')
-EMAIL_PORT = config('EMAIL_PORT', default=587, cast=int)
-EMAIL_HOST_USER = config('EMAIL_HOST_USER', default='apikey')
-EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', default=config('SENDGRID_API_KEY', default='').strip())
-EMAIL_USE_TLS = config('EMAIL_USE_TLS', default=True, cast=bool)
+DEFAULT_FROM_EMAIL = config(
+    'DEFAULT_FROM_EMAIL',
+    default='yourgmail@gmail.com'
+)
+
+EMAIL_HOST = config(
+    'EMAIL_HOST',
+    default='smtp.gmail.com'
+)
+
+EMAIL_PORT = config(
+    'EMAIL_PORT',
+    default=587,
+    cast=int
+)
+
+EMAIL_HOST_USER = config(
+    'EMAIL_HOST_USER',
+    default=''
+)
+
+EMAIL_HOST_PASSWORD = config(
+    'EMAIL_HOST_PASSWORD',
+    default=''
+).strip()
+
+EMAIL_USE_TLS = config(
+    'EMAIL_USE_TLS',
+    default=True,
+    cast=bool
+)
 FRONTEND_URL = config("FRONTEND_URL", default="http://localhost:5173")
 PAYSTACK_PUBLIC_KEY = config("PAYSTACK_PUBLIC_KEY", default="")
 PAYSTACK_SECRET_KEY = config("PAYSTACK_SECRET_KEY", default="")
